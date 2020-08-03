6776fdf1a0bee639d28a4a84dd1735ccccb3d66a
xluo62 <xluo62@uwo.ca>
2020-08-03 09:35:06 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/bundle.js'
+    },
+    //stream bundle into dist folder. not copy them in the disk
+    //looking for html to inject script that has the same dir with output dir
+    devServer: {
+        contentBase: './dist'
+    },
+    //copy src html into dist and inject script automatically
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ]
+}
